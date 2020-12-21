281a72fc2035091777c82a02ca2eae04d7317c18
memellaguno <memellaguno@gmail.com>
2020-12-21 12:52:09 -0600

    data working correctly

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import Model from "./model.js";
-
-const model = Model();
-
-model.getData();
-
 import "./styles.css";
-console.log("change");
+
+(async function main() {
+	const model = Model();
+
+	let weatherData = await model.getData();
+	console.log(weatherData);
+
+	//Query Selectors
+	let dataText = document.querySelector(".dataText p");
+
+	//Create P
+	let p = document.createElement("p");
+	let icon = document.createElement("img");
+
+	//HTML
+	icon.src = weatherData.icon;
+
+	p.innerHTML = weatherData.temp;
+	p.appendChild(icon);
+
+	p.innerHTML += `<br><br>Also, humidity is ${weatherData.humidity}, feels like ${weatherData.feels}, and wind is ${weatherData.wind}`;
+
+	//Append
+	dataText.appendChild(p);
+})();
